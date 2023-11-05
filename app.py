6b8e732009e5c80e31f691dead25c902bf3b4c72
Vincent Rossini <vincent.rossini@gmail.com>
2023-11-05 19:51:57 +1100

    Dockerfile version

--- app.py
+++ app.py
@@ -216,4 +216,4 @@ atexit.register(lambda: scheduler.shutdown())
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
