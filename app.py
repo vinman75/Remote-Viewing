1af36903bc435eb2d488db3e3af5bca4fc1d1db2
Vincent Rossini <vincent.rossini@gmail.com>
2023-12-05 07:35:33 +1100

    update port in file to 5000

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ atexit.register(lambda: scheduler.shutdown())
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6002, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
